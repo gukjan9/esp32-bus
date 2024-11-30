45a1ca358e89a7712d7437e47226f7d6763f1006
GukJang <9ukjang@gmail.com>
2024-11-30 23:15:41 +0900

    8MD06INKM 예제 코드2

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-import framebuf, futaba_8md06inkm, time
-from machine import Pin, freq, SPI
+from machine import SPI, Pin
+from _8MD06INKM import _8MD06INKM
+import time
 
-freq(240000000)
+rest_pin = Pin(27, Pin.OUT)
+cs_pin = Pin(28, Pin.OUT)
+hv_en_pin = Pin(29, Pin.OUT)
 
-hspi = SPI(1, baudrate=5000000, polarity=0, phase=0)
+spi = SPI(
+    id=1,
+    baudrate=100_000,
+    polarity=1,
+    phase=1,
+    sck=Pin(11, Pin.OUT),
+    mosi=Pin(10, Pin.OUT)
+)
 
-en = Pin(24, Pin.OUT)
-rst = Pin(23, Pin.OUT)
-cs = Pin(8, Pin.OUT)
+display = _8MD06INKM(spi, rest_pin, cs_pin, hv_en_pin)
+display.init()
 
-display = futaba_8md06inkm.VFD(hspi, rst, cs, en)
+display.set_brightness(100)
+time.sleep(1)
+display.set_brightness(255)
 
-display.clear()
-display.text("Hello, VFD!", 0, 0)
-display.show()
+display.hv_off()
+time.sleep(1)
+display.hv_on()
 
-time.sleep(5)
+def print_all(chars):
+    for i in range(8):
+        if type(chars[i]) == int:
+            display.code(i, chars[i])
+        else:
+            display.print_char(i, chars[i])
 
-display.clear()
-display.show()
+print_all('Hello-  ')
+
+bits = [0xff, 0x41, 0x41, 0x41, 0xff]
+display.print_bits(7, bits)
