d8107221747587996d97a05284e29ea46df33af6
GukJang <9ukjang@gmail.com>
2024-12-01 23:33:29 +0900

    VFD 작동 확인

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-from machine import SPI, Pin
-from _8MD06INKM import _8MD06INKM
-import time
+import framebuf, futaba_8md06inkm, time
+from machine import Pin, freq, SPI
 
-rest_pin = Pin(27, Pin.OUT)
-cs_pin = Pin(28, Pin.OUT)
-hv_en_pin = Pin(29, Pin.OUT)
+freq(240000000)
 
-spi = SPI(
-    id=1,
-    baudrate=100_000,
-    polarity=1,
-    phase=1,
-    sck=Pin(11, Pin.OUT),
-    mosi=Pin(10, Pin.OUT)
-)
+hspi = SPI(1, baudrate=5000000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(23))
 
-display = _8MD06INKM(spi, rest_pin, cs_pin, hv_en_pin)
-display.init()
+en = Pin(4, Pin.OUT)
+rst = Pin(5, Pin.OUT)
+cs = Pin(26, Pin.OUT)
 
-display.set_brightness(100)
-time.sleep(1)
-display.set_brightness(255)
+display = futaba_8md06inkm.VFD(hspi, rst, cs, en)
 
-display.hv_off()
-time.sleep(1)
-display.hv_on()
-
-def print_all(chars):
-    for i in range(8):
-        if type(chars[i]) == int:
-            display.code(i, chars[i])
-        else:
-            display.print_char(i, chars[i])
-
-print_all('Hello-  ')
-
-bits = [0xff, 0x41, 0x41, 0x41, 0xff]
-display.print_bits(7, bits)
+display.text('Hello', 0, 0)
+display.show()
