786b0780b896d5bc963afe2ce24e3ef7bbb5efc1
GukJang <9ukjang@gmail.com>
2024-12-15 23:29:14 +0900

    xml 파싱 및 데이터 추출

--- bus.py
+++ bus.py
@@ -5,4 +5,41 @@ def get_data(api, id):
     full_url = url + "?serviceKey=" + api + "&routeId=" + id 
 
     response = requests.get(full_url)
-    print(response.content)
+    xml_data = response.content.decode('utf-8')
+    
+    return(xml_data)
+
+def parse_xml(xml):
+    tag = None
+    value = None
+    elements = []
+
+    in_tag = False
+    for char in xml:
+        if char == '<':
+            in_tag = True
+            if value and tag:
+                elements.append((tag, value.strip()))
+                value = None
+            tag = ''
+        elif char == '>':
+            in_tag = False
+            value = ''
+        elif in_tag:
+            tag += char
+        else:
+            value += char
+
+    if value and tag:
+        elements.append((tag, value.strip()))
+
+    query_time = None
+    first_station_id = None
+    for tag, val in elements:
+        if tag == "queryTime":
+            query_time = val
+        elif tag == "stationId" and first_station_id is None:
+            first_station_id = val
+            break
+
+    return query_time, first_station_id
