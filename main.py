50449fb52b6c6d958fba3fbd04177cfddab4f2a1
GukJang <9ukjang@gmail.com>
2024-12-16 00:34:31 +0900

    버스의 현재 위치 받아오기

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from machine import Pin, freq, SPI
 import time, json, framebuf
 import wifi, bus
 
+R9030 = ['229001697', '229001669', '229001668', '229001666', '229000886', '229000885', '229000852', '229000851',
+         '229000850', '229000849', '229000826', '229000557', '229001803', '229001394', '229001659', '229001545',
+         '229000937', '229001553', '229001552']
+
+R7625 = ['229000940', '229000515', '229000664', '229002348', '229001582', '229001583', '229001497', '229001552']
+
+departure = '229001552'
+
 with open('config.json', 'r') as e:
         config = json.load(e)
 
@@ -16,7 +24,35 @@ rst = Pin(5, Pin.OUT)
 cs = Pin(26, Pin.OUT)
 display = futaba_8md06inkm.VFD(hspi, rst, cs, en)
 
-queryTime, stationId = bus.parse_xml(bus.get_data(config['api_key'], config['ID_9030']))
-print(f"Query Time: {queryTime}, First Station ID: {stationId}")
+def where_is_bus(stationId, route, departure):
+    if stationId not in route:
+        return 'None'
+    
+    if stationId == departure:
+        return ' Arr'
+    
+    current_index = route.index(stationId)
+    departure_index = route.index(departure)
+    difference = departure_index - current_index
+    
+    return ' '+ str(difference) + 's'
 
-display.display_str(0, queryTime)
+def display_route(route_name, route):
+    queryTime, stationId = bus.parse_xml(bus.get_data(config['api_key'], config[f'ID_{route_name}']))
+    print(f"Query Time: {queryTime}, First Station ID: {stationId}")
+
+    queryTime = queryTime[11:19]
+    now = where_is_bus(stationId, route, departure)
+    print(now)
+
+    display.fill(0)
+    display.display_str(0, queryTime)
+    time.sleep(2)
+    display.fill(0)
+    display.display_str(0, route_name)
+    display.display_str(4, str(now))
+    time.sleep(3)
+
+while True:
+    display_route('9030', R9030)
+    display_route('7625', R7625)
